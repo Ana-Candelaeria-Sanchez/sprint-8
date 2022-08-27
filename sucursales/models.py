8d9f5a0d02d3d370dc914080a028e827f4319c39
Ana Sanchez <asnchez@frba.utn.edu.ar>
2022-08-27 16:09:13 -0300

    Agrego migracion inicial

--- sucursales/models.py
+++ sucursales/models.py
@@ -7,7 +7,8 @@ class Sucursal(models.Model):
     branch_id = models.AutoField(primary_key=True)
     branch_number = models.BinaryField()
     branch_name = models.TextField()
-    branch_address_id = models.IntegerField()
+    branch_address_id = models.OneToOneField('direcciones.Direccion', models.DO_NOTHING, db_column='branch_address_id',
+                                             blank=True, null=True, default=None)
 
     class Meta:
         db_table = 'sucursal'
